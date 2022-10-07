e904d34c5b009fa1fcd70848278a85851fc120d7
Rebekah Taylor <rebekahtdesigns@gmail.com>
2022-10-07 11:19:17 -0500

    commit all

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (allClay of clay) {
+   const mug = `${allClay} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
